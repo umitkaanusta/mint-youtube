ff0c6b90b7fada568eb8b71bcf4b0ac14c2b6bd3
Ümit Kaan Usta <u.kaanusta@gmail.com>
2020-09-01 03:11:12 +0300

    Added orca executable to test func

--- mint/tests/test_visual_report.py
+++ mint/tests/test_visual_report.py
@@ -1,5 +1,6 @@
 # Functions to test the visual report
 
+from plotly.io.orca import config
 from mint._test_utils import video_ids
 from mint.video_data import get_comments_from_csv
 from mint.util import clean_dir
@@ -10,6 +11,7 @@ import os
 def test_visuals_created():
     df = get_comments_from_csv(video_id=video_ids["en"])
     clean_dir("mint/static/img")
+    config.executable = "mint/orca/orca.exe"
     create_visuals(df, "en", True)
     images = set(os.listdir("mint/static/img"))
     filenames = set(get_visuals_filenames().values())
